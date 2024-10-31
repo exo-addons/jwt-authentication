ac839905a238488a9ede41498a8f1d6caf15f98d
Romain Dénarié <romain.denarie@exoplatform.com>
2024-10-31 13:30:30 +0100

    Fix public key read

--- services/src/main/java/org/exoplatform/addons/jwt/JwtLoginModule.java
+++ services/src/main/java/org/exoplatform/addons/jwt/JwtLoginModule.java
@@ -72,7 +72,7 @@ public class JwtLoginModule extends TomcatLoginModule {
   private void loadPublicKeyContent() {
     try (InputStream is = new URI(this.jwtPublicKeyUrl).toURL().openStream()) {
       jwtPublicKeyContent = IOUtils.toString(is, StandardCharsets.UTF_8);
-      jwtPublicKeyContent = jwtPublicKeyContent.replace("\\n", "")
+      jwtPublicKeyContent = jwtPublicKeyContent.replace("\n", "")
                                                .replace("-----BEGIN PUBLIC KEY-----", "")
                                                .replace("-----END PUBLIC KEY-----", "");
       LOG.info("Public key founded : {}", jwtPublicKeyContent);
